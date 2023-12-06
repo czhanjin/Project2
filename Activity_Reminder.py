01a8c3b3126add6e892b0f3b3f4aca65f18ce1c4
czhanjin <you@example.com>
2023-12-06 16:04:44 -0500

    Med and Activity reminder 4.0

---
+++ Activity_Reminder.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import threading
+import pygame
+
+
+def close_window():
+    root.destroy()
+
+# Tkinter window
+root = tk.Tk()
+root.title("Activity Reminder")
+
+message_label = tk.Label(root, text="Hi, Mr. Li! It's time to move your ass. Shake ur ass ;)", font=("Helvetica", 30))
+message_label.pack(pady=20)
+
+# Button to close the window and stop audio
+close_button = tk.Button(root, text="OK", command=close_window, font=("Helvetica", 18))
+close_button.pack(pady=10)
+
+# Pop-up Window size
+root.geometry("600x200")
+
+root.mainloop()
