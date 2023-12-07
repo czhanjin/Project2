b5b72b4eb85ccda19f1537a0565a88f1b86fa8fc
anht-nguyen <tuananh.nguyen28898@gmail.com>
2023-12-07 15:52:36 -0500

    add script to control servo with crontab for reminders

---
+++ control-servo/control-servo.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>
+int servoPin = 3;
+
+Servo Servo1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Servo1.attach(servoPin);
+  Servo1.write(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0){
+    int data = Serial.read() - '0';
+
+    switch (data){
+      case 1:
+        moveServo();
+        break;
+      default:
+        break;
+      }
+    }
+
+}
+
+void moveServo(){
+  Servo1.write(0);
+  delay(500);
+  Servo1.write(90);
+  delay(500);
+  Servo1.write(0);
+  delay(500);
+  Servo1.write(90);
+  delay(500);
+  }
